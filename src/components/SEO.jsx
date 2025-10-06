9f6aa44dea19185d9c860a23810e642e8fb25515
Patrick Harris <pjamesharris84@gmail.com>
2025-10-06 16:59:21 +0100

    fix seo page

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,4 +1,4 @@
-import { Helmet } from "react-helmet-async";
+import React from "react";
 
 export default function SEO({
   title = "PJH Web Services | Bespoke Websites & CRM Systems",
@@ -7,13 +7,13 @@ export default function SEO({
   url = "https://www.pjhwebservices.co.uk",
 }) {
   return (
-    <Helmet>
-      {/* Core SEO */}
+    <>
+      {/* ✅ Core SEO */}
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="canonical" href={url} />
 
-      {/* OpenGraph / Facebook */}
+      {/* ✅ OpenGraph / Facebook */}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
@@ -21,15 +21,15 @@ export default function SEO({
       <meta property="og:url" content={url} />
       <meta property="og:site_name" content="PJH Web Services" />
 
-      {/* Twitter */}
+      {/* ✅ Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
 
-      {/* Theme & Icons */}
+      {/* ✅ Theme & Icons */}
       <meta name="theme-color" content="#007BFF" />
       <link rel="icon" href="/favicon.ico" />
-    </Helmet>
+    </>
   );
 }
